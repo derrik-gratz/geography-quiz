447941159751f103ca619395e0f890ec9ac485bd
derrik-gratz <derrik.gratz@gmail.com>
2026-02-10 11:38:12 -0800

    cleanup lint/prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,89 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import json from "@eslint/json";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import json from '@eslint/json';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { ignores: ["node_modules/", "dist/"] },
+  { ignores: ['node_modules/', 'dist/', 'eslint.config.js'] },
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js, react: pluginReact, "react-hooks": pluginReactHooks },
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js, react: pluginReact, 'react-hooks': pluginReactHooks },
     ...js.configs.recommended,
     ...pluginReact.configs.flat.recommended,
-    ...pluginReact.configs.flat["jsx-runtime"],
-    ...pluginReactHooks.configs["recommended-latest"],
+    ...pluginReact.configs.flat['jsx-runtime'],
+    ...pluginReactHooks.configs['recommended-latest'],
     languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
       globals: { ...globals.browser, ...globals.node },
-      parserOptions: { ecmaFeatures: { jsx: true } },
     },
-    settings: { react: { version: "detect" } },
+    settings: { react: { version: 'detect' } },
     rules: {
-      "react/prop-types": "off",
+      'react/prop-types': 'off',
     },
   },
   {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json/json",
-    extends: ["json/recommended"],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { '@typescript-eslint': tseslint },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase', 'snake_case'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        { selector: 'variable', modifiers: ['destructured'], format: null },
+        {
+          selector: 'parameter',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'allow',
+        },
+        { selector: 'import', format: ['camelCase', 'PascalCase'] },
+        { selector: 'typeLike', format: ['PascalCase'] },
+        { selector: 'function', format: ['camelCase', 'PascalCase'] },
+        {
+          selector: 'objectLiteralProperty',
+          format: ['camelCase', 'snake_case', 'UPPER_CASE'],
+        },
+      ],
+    },
   },
   {
-    files: ["**/*.css"],
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.css'],
     plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
+    language: 'css/css',
+    extends: ['css/recommended'],
   },
 ]);
