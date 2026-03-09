e33b1cf3e1b06987cb7d9bdbfcd39958ad185ee0
derrik-gratz <derrik.gratz@gmail.com>
2026-03-09 13:20:23 -0700

    z axis warning

--- src/features/profile/components/ScoreTimeline.jsx
+++ src/features/profile/components/ScoreTimeline.jsx
@@ -90,6 +90,7 @@ export function ScoreTimeline({ userData }) {
         Plot.line(plotData, {
           x: 'date',
           y: 'value',
+          z: 'metric',
           stroke: 'metric',
           strokeWidth: 2,
           curve: 'linear',
@@ -98,6 +99,7 @@ export function ScoreTimeline({ userData }) {
           x: 'date',
           y: 'value',
           stroke: 'metric',
+          z: 'metric',
           r: 4,
           title: (d) =>
             `${d.date.toLocaleDateString('en-GB', {
